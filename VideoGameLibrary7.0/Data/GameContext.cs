69d4322d25f32a009d51e24e67190ff51112687d
JeremieCius <jeremiecius@gmail.com>
2023-08-29 22:48:50 -0600

    ok for real login fixed

--- VideoGameLibrary7.0/Data/GameContext.cs
+++ VideoGameLibrary7.0/Data/GameContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using VideoGameLibrary_PartOne.Models;
 using VideoGameLibrary7._0.Areas.Identity.Data;
 
 namespace VideoGameLibrary7._0.Data
 {
-    public class GameContext : DbContext
+    public class GameContext : IdentityDbContext<GameUser>
     {
 
         public GameContext(DbContextOptions options) : base(options)
--- VideoGameLibrary7.0/Migrations/20230830044800_UserClaimSchema.Designer.cs
+++ VideoGameLibrary7.0/Migrations/20230830044800_UserClaimSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VideoGameLibrary7._0.Data;
 
@@ -11,9 +12,11 @@ using VideoGameLibrary7._0.Data;
 namespace VideoGameLibrary7._0.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20230830044800_UserClaimSchema")]
+    partial class UserClaimSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
